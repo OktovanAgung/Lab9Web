7b8add68bed56249b16fc2f8275a952c0fb2b473
oktovan Agung Shaiendra <72904723+OktovanAgung@users.noreply.github.com>
2022-05-30 17:23:37 +0700

    docs: update README.md

---
+++ lab8_php_database/layouts/footer.php
@@ -0,0 +1,6 @@
+        <footer class="mt-4 pt-4">
+            <p>&copy; 2022 | Oktovan Agung Shailendra </p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ lab8_php_database/layouts/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Data Barang</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Data Barang</h1>
+    <a href="tambah.php">Tambah Barang</a>
